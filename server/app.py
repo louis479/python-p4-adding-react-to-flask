1437da3ddf107c1b1b9f5616cf738bc06da9cdc5
louis-ngugi <louisngugi858@gmail.com>
2025-06-30 01:34:54 +0300

    adding react to flask

--- server/app.py
+++ server/app.py
@@ -9,8 +9,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-CORS(app)
-migrate = Migrate(app, db)
+# CORS(app)
+# migrate = Migrate(app, db)
 
 db.init_app(app)
 
